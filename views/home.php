e48f669ce87c5ec1d21295c10707fc71598d0af9
Gustavo <52762631+devgustaff@users.noreply.github.com>
2021-01-18 19:32:46 -0300

    Update home.php
    updated home and css files

--- views/home.php
+++ views/home.php
@@ -1,15 +1,9 @@
 <section class="box-login">
   <div class="login">
     <form action="" method="post">
-      <div class="form-group">
-        <input type="text" name="login" placeholder="Usuário">
-      </div>
-      <div class="form-group">
-        <input type="password" name="password" placeholder="Senha">
-      </div>
-      <div class="form-group">
-        <button type="submit">Logar</button>
-      </div>
+      <input type="text" name="login" placeholder="Usuário">
+      <input type="password" name="password" placeholder="Senha">
+      <button type="submit">Logar</button>
     </form>
   </div>
 </section>
