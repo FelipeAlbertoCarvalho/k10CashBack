3f0797004abbabb06625699f4e7ea2151780b107
Gustavo <52762631+devgustaff@users.noreply.github.com>
2021-01-22 00:33:00 -0300

    Update and rename Login.php to loginModel.php
    Refatorando logica login

---
+++ models/loginModel.php
@@ -0,0 +1,50 @@
+<?php
+
+class LoginModel extends Mysql {
+  protected $email;
+  protected $senha;
+  
+  public function getLoginCliente()
+  {
+    $sql = "SELECT * 
+            FROM cliente
+            WHERE email='{$this->getEmail()}'
+            AND senha='{$this->getSenha()}'";
+
+    $sql = $this->conn->query($sql);
+
+    if ($sql->rowCount() > 0) {
+      $sql = $sql->fetch();
+      $_SESSION['id_cliente'] = $sql['id'];
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  public function getLoginCliente()
+  {
+    $sql = "SELECT * 
+            FROM loja
+            WHERE email='{$this->getEmail()}'
+            AND senha='{$this->getSenha()}'";
+
+    $sql = $this->conn->query($sql);
+
+    if ($sql->rowCount() > 0) {
+      $sql = $sql->fetch();
+      $_SESSION['id_loja'] = $sql['id'];
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  public function getEmail() {
+    return $this->email;
+  }
+  
+  public function setSenha($senha) {
+    return $this->senha = $senha;
+  }
+}
